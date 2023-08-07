c97c5a2b955943f832568e00b01e47799615c69d
Kartikesh <kartikesh.sharma@capanicus.com>
2023-08-07 18:02:30 +0530

    updates

--- components/area-container.js
+++ components/area-container.js
@@ -1,12 +1,13 @@
 import Link from "next/link";
+import {useRouter} from "next/router";
 let property_by_area=[
   {text:"Centerville", value:"25 listings"},
   {text:"Centerville", value:"25 listings"},
   {text:"Centerville", value:"25 listings"},
  
 ]
-
 const AreaContainer = () => {
+  const router=useRouter()
   return (
     <div className="self-stretch flex-1 flex flex-col py-[53px] px-[50px] items-center justify-start gap-[45px] text-center text-21xl text-primary-800 font-body-regular-600">
       <div className="self-stretch flex flex-col py-0 px-[30px] items-center justify-start gap-[24px] md:self-stretch md:w-auto">
@@ -21,9 +22,10 @@ const AreaContainer = () => {
       <div className="self-stretch flex-1 flex flex-row flex-wrap py-0 px-2.5 box-border items-start justify-center max-w-[95%px] text-left text-5xl text-gray-white">
         <div className="self-stretch flex-1 flex flex-col items-center justify-start gap-[24px] max-w-[1300px]">
           <div className="self-stretch flex flex-row items-start justify-center gap-[26px] lg:flex-row md:flex-col">
-          {property_by_area.map(val=> <Link
+          {property_by_area.map((val,i)=> <Link
               className="cursor-pointer [text-decoration:none] flex-1 rounded-lg h-[241px] flex flex-row items-start justify-start relative bg-[url(/card-1@3x.png)] bg-cover bg-no-repeat bg-[top] text-[inherit] md:flex-[unset] md:self-stretch"
-              href="/"
+              href="/Details"
+              
             >
               <div className="my-0 mx-[!important] absolute top-[16px] left-[16px] flex flex-col items-start justify-start gap-[11px] z-[0]">
                 <div className="relative leading-[32px] font-semibold">
